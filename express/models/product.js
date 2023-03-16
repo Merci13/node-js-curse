44a888f7695bb16804c216c1f44ffe6ef39cfaff
Merci13 <mrjorxe@gmail.com>
2023-03-15 21:47:38 -0600

    was added sequelize to the project

--- express/models/product.js
+++ express/models/product.js
@@ -1,125 +1,164 @@
 
-//--We don't need this any more because we will use database
-// const fs = require('fs');
-// const path = require('path');
+// //--We don't need this any more because we will use database
+// // const fs = require('fs');
+// // const path = require('path');
 
-// const p = path.join(
-//     path.dirname(require.main.filename),
-//     'data',
-//     'products.json'
-// );
-//-------------------------------------------------------------------------------------//
-
-
-
-const database = require('../utils/database');
-const Cart = require('./cart');
-
-//--We don't need this any more because we will use database
-// const getProductsFromFile = callBack => {
+// // const p = path.join(
+// //     path.dirname(require.main.filename),
+// //     'data',
+// //     'products.json'
+// // );
+// //-------------------------------------------------------------------------------------//
 
 
-//     fs.readFile(p, (err, fileContent) => {
-//         if (err) {
-//             return callBack([]);
-//         }
-//         callBack(JSON.parse(fileContent));
-//     });
+
+// const database = require('../utils/database');
+// const Cart = require('./cart');
+
+// //--We don't need this any more because we will use database
+// // const getProductsFromFile = callBack => {
+
+
+// //     fs.readFile(p, (err, fileContent) => {
+// //         if (err) {
+// //             return callBack([]);
+// //         }
+// //         callBack(JSON.parse(fileContent));
+// //     });
+// // }
+// //-------------------------------------------------------------------------------------//
+
+
+
+// module.exports = class Product {
+
+//     constructor(id, title, imageUrl, description, price) {
+//         this.title = title;
+//         this.imageUrl = imageUrl;
+//         this.description = description;
+//         this.price = price;
+//         this.id = id;
+//     }
+
+
+
+//     save() {
+//         // products.push(this); //'this' words make reference of the variable in the constructor method
+
+
+//         //--We don't need this any more because we will use database
+//         // getProductsFromFile(products => {
+//         //     //check if the id already exist
+//         //     if (this.id) {
+//         //         const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+//         //         const updatedProducts = [...products];
+//         //         updatedProducts[existingProductIndex] = this;
+//         //         fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+//         //             console.log(err);
+//         //         });
+//         //     } else {
+//         //         this.id = Math.random().toString();
+//         //         products.push(this);
+
+//         //         fs.writeFile(p, JSON.stringify(products), (err) => {
+//         //             console.log(err, 'Error in "save" method in product.js. <<<<<---------------------------');
+//         //         });
+//         //     }
+
+//         // });
+
+//         //-------------------------------------------------------------------------------------//
+
+//        return database.execute('INSERT INTO products(title, price, description,imageUrl) VALUES(?,?,?,?)',
+//         [this.title, this.price, this.description, this.imageUrl]);
+
+
+//     }
+
+//     static deleteById(id) {
+
+//         //--We don't need this any more because we will use database
+//         // getProductsFromFile(products => {
+
+//         //     const product = products.find(prod => prod.id === id);
+//         //     const updateProducts = products.filter(prod => prod.id !== id);
+//         //     fs.writeFile(p, JSON.stringify(updateProducts), err => {
+//         //         if (!err) {
+//         //             Cart.deleteProduct(id, product.price);
+//         //         }
+//         //     });
+
+
+//         // });
+//         //-------------------------------------------------------------------------------------//
+
+
+//     }
+
+//     static fetchAll(
+//         //callBack
+//         ) {
+//         //--We don't need this any more because we will use database
+//         //  getProductsFromFile(callBack);
+//         //-------------------------------------------------------------------------------------//
+
+//       return  database.execute('SELECT * FROM products');
+
+//     }
+
+
+//     static findById(
+//         id,
+//          //callBack
+//          ) {
+//         //--We don't need this any more because we will use database
+//         // getProductsFromFile(products => {
+//         //     const product = products.find(p => p.id === id);
+//         //     callBack(product);
+//         // });
+//         //-------------------------------------------------------------------------------------//
+    
+//         return database.execute('SELECT * FROM products WHERE products.id = ?', [id]);
+    
+//     }
+
 // }
-//-------------------------------------------------------------------------------------//
+
+
+//-------------------------Using Sequelize---------------------------//
+
+const Sequelize = require('sequelize');
+const sequelize = require('../utils/database');
 
 
 
-module.exports = class Product {
+const Product = sequelize.define(
+    'product',
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+        title: {
+            type: Sequelize.STRING
+        },
+        price: {
+            type: Sequelize.DOUBLE,
+            allowNull: false,
 
-    constructor(id, title, imageUrl, description, price) {
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
-        this.id = id;
+        },
+        imageUrl: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        description: {
+            type: Sequelize.STRING,
+            allowNull: false,
+
+        }
     }
+)
 
-
-
-    save() {
-        // products.push(this); //'this' words make reference of the variable in the constructor method
-
-
-        //--We don't need this any more because we will use database
-        // getProductsFromFile(products => {
-        //     //check if the id already exist
-        //     if (this.id) {
-        //         const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-        //         const updatedProducts = [...products];
-        //         updatedProducts[existingProductIndex] = this;
-        //         fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-        //             console.log(err);
-        //         });
-        //     } else {
-        //         this.id = Math.random().toString();
-        //         products.push(this);
-
-        //         fs.writeFile(p, JSON.stringify(products), (err) => {
-        //             console.log(err, 'Error in "save" method in product.js. <<<<<---------------------------');
-        //         });
-        //     }
-
-        // });
-
-        //-------------------------------------------------------------------------------------//
-
-       return database.execute('INSERT INTO products(title, price, description,imageUrl) VALUES(?,?,?,?)',
-        [this.title, this.price, this.description, this.imageUrl]);
-
-
-    }
-
-    static deleteById(id) {
-
-        //--We don't need this any more because we will use database
-        // getProductsFromFile(products => {
-
-        //     const product = products.find(prod => prod.id === id);
-        //     const updateProducts = products.filter(prod => prod.id !== id);
-        //     fs.writeFile(p, JSON.stringify(updateProducts), err => {
-        //         if (!err) {
-        //             Cart.deleteProduct(id, product.price);
-        //         }
-        //     });
-
-
-        // });
-        //-------------------------------------------------------------------------------------//
-
-
-    }
-
-    static fetchAll(
-        //callBack
-        ) {
-        //--We don't need this any more because we will use database
-        //  getProductsFromFile(callBack);
-        //-------------------------------------------------------------------------------------//
-
-      return  database.execute('SELECT * FROM products');
-
-    }
-
-
-    static findById(
-        id,
-         //callBack
-         ) {
-        //--We don't need this any more because we will use database
-        // getProductsFromFile(products => {
-        //     const product = products.find(p => p.id === id);
-        //     callBack(product);
-        // });
-        //-------------------------------------------------------------------------------------//
-    
-        return database.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-    
-    }
-
-}
+module.exports = Product;
--- express/utils/database.js
+++ express/utils/database.js
@@ -1,14 +1,33 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-//create a pool of conections to increase the performance 
+// //create a pool of conections to increase the performance 
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
 
-    host: 'localhost', // the host where the data base is hosted
-    user: 'root', //this is by default, it can be change as you need it
-    database: 'node-complete', //database name
-    password: 'need1992'
+//     host: 'localhost', // the host where the data base is hosted
+//     user: 'root', //this is by default, it can be change as you need it
+//     database: 'node-complete', //database name
+//     password: 'need1992'
     
-});
+// });
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+//----------------------------------------------------------------------//
+
+//In order to use squelize package we need to 
+//configure it to use mysql2 package and make the conecction with the data base
+//
+
+const Sequelize = require('sequelize'); // when the constant is capitalize means that we are using a constructor;
+
+
+const sequelize = new Sequelize(
+    'node-complete',//data base name
+    'root', // username from the data base
+    'need1992',// password
+    {dialect: 'mysql',
+    host: 'localhost'
+    }
+);
+module.exports = sequelize;
