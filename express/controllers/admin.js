192dbaa0150f011e5dfbb8182b2929f9c8741ebe
Merci13 <mrjorxe@gmail.com>
2023-04-15 14:57:03 -0600

    admin js changes

--- express/controllers/admin.js
+++ express/controllers/admin.js
@@ -53,148 +53,148 @@ exports.postAddProduct = (req, res, next) => {
 
 };
 
-exports.getEditProduct = (req, res, next) => {
-    //res.sendFile(path.join(rootDir,'views', 'add-product.html'))//for routing with html 
+// exports.getEditProduct = (req, res, next) => {
+//     //res.sendFile(path.join(rootDir,'views', 'add-product.html'))//for routing with html 
 
-    //checkin for optionals queryparameters 
-    // always the extract value will be a string value, soo, this value req.query.edit, will return "false" or "true" insted of 
-    // false or true
+//     //checkin for optionals queryparameters 
+//     // always the extract value will be a string value, soo, this value req.query.edit, will return "false" or "true" insted of 
+//     // false or true
 
-    const editMode = req.query.edit;
+//     const editMode = req.query.edit;
 
-    if (!editMode) {
-        return res.redirect('/');
-    }
-    //fetch the product by id
-    const productId = req.params.productId;
-    // Product.findById(productId, product => {
-    //     if (!product) {
-    //         return res.redirect('/');
-    //     }
-    //     res.render(
-    //         'admin/edit-product',
-    //         {
+//     if (!editMode) {
+//         return res.redirect('/');
+//     }
+//     //fetch the product by id
+//     const productId = req.params.productId;
+//     // Product.findById(productId, product => {
+//     //     if (!product) {
+//     //         return res.redirect('/');
+//     //     }
+//     //     res.render(
+//     //         'admin/edit-product',
+//     //         {
 
-    //             pageTitle: 'Edit Product',
-    //             path: '/edit-product',
-    //             editing: editMode,
-    //             product: product
+//     //             pageTitle: 'Edit Product',
+//     //             path: '/edit-product',
+//     //             editing: editMode,
+//     //             product: product
 
 
-    //         });
-    // });
+//     //         });
+//     // });
 
-    //-------Sequelize---------//
-    //Note: replace all ocurrences of .findById() to .findByPk();
+//     //-------Sequelize---------//
+//     //Note: replace all ocurrences of .findById() to .findByPk();
 
-    req.user.getProducts({where: { id: productId}})
+//     req.user.getProducts({where: { id: productId}})
 
-   // Product.findByPk(productId)
+//    // Product.findByPk(productId)
     
-    .then(products => {
+//     .then(products => {
 
-        const product = products[0];
+//         const product = products[0];
 
-        if (!product) {
-            return res.redirect('/');
-        }
-        res.render(
-            'admin/edit-product',
-            {
-                pageTitle: 'Edit Product',
-                path: '/edit-product',
-                editing: editMode,
-                product: product
-
-
-            });
-
-    }).catch(err => console.log('Error: ', err, "------------------->>>"));
+//         if (!product) {
+//             return res.redirect('/');
+//         }
+//         res.render(
+//             'admin/edit-product',
+//             {
+//                 pageTitle: 'Edit Product',
+//                 path: '/edit-product',
+//                 editing: editMode,
+//                 product: product
 
 
+//             });
 
-};
-
-exports.getProducts = (req, res, nex) => {
-
-    // const products = Product.fetchAll((products)=>{
-    //     res.render('admin/products',
-    //     {
-    //        prod: products,
-    //        pageTitle: 'Admin Product',
-    //        path: '/admin-products',
-    //        activeShop: true,
-
-    //     });
-    // });
-
-    //----------Sequelize----------//
-    req.user.getProducts()
-
-    // Product.findAll()
-    .then(products => {
-        res.render('admin/products',
-            {
-                prod: products,
-                pageTitle: 'Admin Product',
-                path: '/admin-products',
-                activeShop: true,
-
-            });
-
-    }).catch(err => console.log('Error: ', err, "-------------------->>>>>>>>>>"));
-
-};
-
-exports.postEditProduct = (req, res, next) => {
-
-    const productId = req.body.productId;
-    const updateTitle = req.body.title;
-    const updatePrice = req.body.price;
-    const updateDescription = req.body.description;
-    const updateImageUrl = req.body.imageUrl;
-
-    // const updateProduct = new Product(productId, updateTitle, updateImageUrl, updateDescription, updatePrice);
-    // updateProduct.save();
-
-    //------Sequelize-----------//
-    Product.findByPk(productId).then(product => {
-
-        product.title = updateTitle;
-        product.price = updatePrice;
-        product.imageUrl = updateImageUrl;
-        product.description = updateDescription;
-        return product.save();
-
-
-    }).then(result => {
-        console.log("Updated Product, Id: ", result.id);
-        res.redirect('/products');
-    }).catch(err => console.log("Error: ", err, " ------------>>>>"))
+//     }).catch(err => console.log('Error: ', err, "------------------->>>"));
 
 
 
+// };
 
-};
+// exports.getProducts = (req, res, nex) => {
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
-    // Product.deleteById(prodId);
-    //-----Sequelize----//
+//     // const products = Product.fetchAll((products)=>{
+//     //     res.render('admin/products',
+//     //     {
+//     //        prod: products,
+//     //        pageTitle: 'Admin Product',
+//     //        path: '/admin-products',
+//     //        activeShop: true,
 
-    Product.findByPk(prodId)
-        .then(product => {
-            console.log(product, ' ----------------->>>');
-            return product.destroy();
-        })
-        .then(result => {
-            console.log("Destroy: ", result, '------------->>>>')
-            res.redirect('/products');
-        })
-        .catch(err => { 
-            console.log("Error: ", err, " ---------->>>");
-            res.redirect('/');
+//     //     });
+//     // });
+
+//     //----------Sequelize----------//
+//     req.user.getProducts()
+
+//     // Product.findAll()
+//     .then(products => {
+//         res.render('admin/products',
+//             {
+//                 prod: products,
+//                 pageTitle: 'Admin Product',
+//                 path: '/admin-products',
+//                 activeShop: true,
+
+//             });
+
+//     }).catch(err => console.log('Error: ', err, "-------------------->>>>>>>>>>"));
+
+// };
+
+// exports.postEditProduct = (req, res, next) => {
+
+//     const productId = req.body.productId;
+//     const updateTitle = req.body.title;
+//     const updatePrice = req.body.price;
+//     const updateDescription = req.body.description;
+//     const updateImageUrl = req.body.imageUrl;
+
+//     // const updateProduct = new Product(productId, updateTitle, updateImageUrl, updateDescription, updatePrice);
+//     // updateProduct.save();
+
+//     //------Sequelize-----------//
+//     Product.findByPk(productId).then(product => {
+
+//         product.title = updateTitle;
+//         product.price = updatePrice;
+//         product.imageUrl = updateImageUrl;
+//         product.description = updateDescription;
+//         return product.save();
+
+
+//     }).then(result => {
+//         console.log("Updated Product, Id: ", result.id);
+//         res.redirect('/products');
+//     }).catch(err => console.log("Error: ", err, " ------------>>>>"))
+
+
+
+
+// };
+
+// exports.postDeleteProduct = (req, res, next) => {
+//     const prodId = req.body.productId;
+//     // Product.deleteById(prodId);
+//     //-----Sequelize----//
+
+//     Product.findByPk(prodId)
+//         .then(product => {
+//             console.log(product, ' ----------------->>>');
+//             return product.destroy();
+//         })
+//         .then(result => {
+//             console.log("Destroy: ", result, '------------->>>>')
+//             res.redirect('/products');
+//         })
+//         .catch(err => { 
+//             console.log("Error: ", err, " ---------->>>");
+//             res.redirect('/');
         
-        });
+//         });
 
-} 
+// } 
